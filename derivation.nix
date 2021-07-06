cd224251236fadedf56e0a8dc838f65455488a59
L <pub-48a13598@entwinder.net>
2021-07-05 21:20:27 -0400

    Support newer nixpkgs

--- derivation.nix
+++ derivation.nix
@@ -5,8 +5,10 @@ stdenv.mkDerivation {
   version = "0.0.1";
   src = lib.cleanSource ./.;
   buildInputs = [ linuxHeaders ];
-  REMARKABLE_VERSION = targetPlatform.platform.rmVersion;
+  REMARKABLE_VERSION = targetPlatform.rmVersion;
   UINPUT_KO = "${linuxPackages.kernel}/lib/modules/${linuxPackages.kernel.modDirVersion}/kernel/drivers/input/misc/uinput.ko";
+  # work around EABI version issues with bfd
+  preConfigure = "LD=$LD.gold";
   outputs = [ "out" "dev" ];
   installPhase = ''
     mkdir -p $out/bin
